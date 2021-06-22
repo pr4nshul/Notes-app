4a9d5ab0904854e15cf4b4cb1dead181144a7372
Pranshul Aggarwal <pranshulaggarwal000@gmail.com>
2021-06-22 05:36:44 +0530

    refactored drawer button

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -8,12 +8,13 @@ class MyDrawer extends StatelessWidget {
   final NoteProvider noteProvider;
 
   const MyDrawer({this.noteProvider});
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Container(
-        width: MediaQuery.of(context).size.width*0.48,
-        height: MediaQuery.of(context).size.height*0.8155,
+        width: MediaQuery.of(context).size.width * 0.48,
+        height: MediaQuery.of(context).size.height * 0.8155,
         child: Drawer(
           elevation: 10,
           child: Container(
@@ -36,114 +37,63 @@ class MyDrawer extends StatelessWidget {
                         fontSize: 18),
                   )),
                 ),
-                Container(
-                  margin: EdgeInsets.fromLTRB(10, 18, 10, 8),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(70),
-                    //   border: Border.all(color: Colors.white,width: 4),
-                    color: Color(0xff191970),
-                  ),
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.grid_view,
-                      color: Colors.white,
-                    ),
-                    title: Text(
-                      "Grid View",
-                      style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.white,
-                          fontWeight: FontWeight.w700),
-                    ),
-                    onTap: () {
-                      noteProvider.gridView();
-                      Navigator.of(context).pop();
-                    },
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.fromLTRB(10, 8, 10, 8),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(70),
-                    //    border: Border.all(color: Colors.white,width: 4),
-                    color: Color(0xff191970),
-                  ),
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.view_list,
-                      color: Colors.white,
-                    ),
-                    title: Text(
-                      "List View",
-                      style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.white,
-                          fontWeight: FontWeight.w700),
-                    ),
-                    onTap: () {
-                      noteProvider.listView();
-                      Navigator.of(context).pop();
-                    },
-                  ),
-                ),
-                // Divider(
-                //   thickness: 2,
-                //   color: Colors.black,
-                // ),
-                Container(
-                  margin: EdgeInsets.fromLTRB(10, 8, 10, 8),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(70),
-                    //  border: Border.all(color: Colors.white,width: 4),
-                    color:Color(0xff191970),
-                  ),
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.home_outlined,
-                      color: Colors.white,
-                    ),
-                    title: Text(
-                      "Home",
-                      style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.white,
-                          fontWeight: FontWeight.w700),
-                    ),
-                    onTap: () => Navigator.pushReplacement(context,
-                        MaterialPageRoute(builder: (context) {
-                      return Home();
-                    })),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.fromLTRB(10, 8, 10, 8),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(70),
-                    //  border: Border.all(color: Colors.white,width: 4),
-                    color: Color(0xff191970),
-                  ),
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.archive_outlined,
-                      color: Colors.white,
-                    ),
-                    title: Text(
-                      "Archive",
-                      style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.white,
-                          fontWeight: FontWeight.w700),
-                    ),
-                    onTap: () => Navigator.pushReplacement(context,
-                        MaterialPageRoute(builder: (context) {
-                      return Consumer<NoteProvider>(builder: (context,noteProvider,__)=> ArchivedNotes(noteProvider:noteProvider),);
-                    })),
-                  ),
-                ),
+                drawerButton(context, Icons.grid_view, "Grid View", () {
+                  noteProvider.gridView();
+                  Navigator.of(context).pop();
+                }, 10, 18, 10, 8),
+                drawerButton(context, Icons.view_list, "List View", () {
+                  noteProvider.listView();
+                  Navigator.of(context).pop();
+                }, 10, 8, 10, 8),
+                drawerButton(
+                    context,
+                    Icons.home_outlined,
+                    "Home",
+                    () => Navigator.pushReplacement(context,
+                            MaterialPageRoute(builder: (context) {
+                          return Home();
+                        })),
+                    10, 8, 10, 8),
+                drawerButton(
+                    context,
+                    Icons.archive_outlined,
+                    "Archive",
+                    () => Navigator.pushReplacement(context,
+                            MaterialPageRoute(builder: (context) {
+                          return Consumer<NoteProvider>(
+                            builder: (context, noteProvider, __) =>
+                                ArchivedNotes(noteProvider: noteProvider),
+                          );
+                        })),
+                    10, 8, 10, 8),
               ],
             ),
           ),
         ),
+      ),
+    );
+  }
+
+  Container drawerButton(BuildContext context, IconData icon, String text,
+      Function func, double left, double top, double right, double bottom) {
+    return Container(
+      margin: EdgeInsets.fromLTRB(left, top, right, bottom),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(70),
+        //   border: Border.all(color: Colors.white,width: 4),
+        color: Color(0xff191970),
+      ),
+      child: ListTile(
+        leading: Icon(
+          icon,
+          color: Colors.white,
+        ),
+        title: Text(
+          text,
+          style: TextStyle(
+              fontSize: 16, color: Colors.white, fontWeight: FontWeight.w700),
+        ),
+        onTap: func,
       ),
     );
   }
